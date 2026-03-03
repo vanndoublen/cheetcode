88fe68895e0b0556136f1e86eeac81a5ff756dcf
vanndoublen <sovannsean9@gmail.com>
2026-03-04 00:12:25 +0800

    fix: delete app/generated folder which causes routing problem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Header } from "@/components/header";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
-export default function Home() {
+export default function Page() {
   return (
     <div className="h-full w-full">
       <ComponentExample />
